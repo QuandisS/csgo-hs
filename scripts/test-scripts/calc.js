88fbeb337131c01004f7de1e8823f5e6f018dec5
QuandisS <anton.kravtsov.31@gmail.com>
2019-02-05 19:23:27 +0300

    working calc in playground

--- scripts/test-scripts/calc.js
+++ scripts/test-scripts/calc.js
@@ -1,1 +1,19 @@
-//var num1 = document.
+// DOM Nodes
+var form = {
+        calc: document.getElementById("Calc"),
+        num1: document.getElementById("num1"),
+        num2: document.getElementById("num2"),
+        result: document.getElementById("calc-result")
+}
+
+// Form submit
+form.calc.addEventListener("submit", calc);
+
+// Calc result calculation
+
+function calc(e){
+    var sum = Number.parseInt(form.num1.value) + Number.parseInt(form.num2.value);
+    form.result.value = sum;
+    console.log("Summed!");
+    e.preventDefault();
+}
