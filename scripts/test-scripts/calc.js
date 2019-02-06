9eca69366466d7aebc805dab76b2ae64fc5f6c3f
QuandisS <anton.kravtsov.31@gmail.com>
2019-02-06 21:36:39 +0300

    different op. and graphic impovments

--- scripts/test-scripts/calc.js
+++ scripts/test-scripts/calc.js
@@ -3,7 +3,8 @@ var form = {
         calc: document.getElementById("Calc"),
         num1: document.getElementById("num1"),
         num2: document.getElementById("num2"),
-        result: document.getElementById("calc-result")
+        result: document.getElementById("calc-result"),
+        op: document.getElementById("operation")
 }
 
 // Form submit
@@ -12,7 +13,16 @@ form.calc.addEventListener("submit", calc);
 // Calc result calculation
 
 function calc(e){
-    var sum = Number.parseInt(form.num1.value) + Number.parseInt(form.num2.value);
+    var sum = 0
+    switch (form.op.value){
+        case "+":
+            sum = Number.parseInt(form.num1.value) + Number.parseInt(form.num2.value)
+            break
+        case "-":
+            sum = Number.parseInt(form.num1.value) - Number.parseInt(form.num2.value)
+            break
+    }
+    
     form.result.value = sum;
     console.log("Summed!");
     e.preventDefault();
